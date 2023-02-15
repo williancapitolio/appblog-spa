27aa2e42b559ec3cc1564fd48695d222f07e74f0
Willian <williansilvap25@gmail.com>
2023-02-14 23:21:05 -0300

    register page styles ok

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+
 export const Register = () => {
     return (
-        <>
-            <h1>
-                Register
-            </h1>
-        </>
+        <div className="auth">
+            <h1>Registrar</h1>
+            <form>
+                <input type="text" name="username" placeholder="Usuário" required />
+                <input type="email" name="email" placeholder="E-mail" required />
+                <input type="password" name="password" placeholder="Senha" required />
+                <button>Registrar</button>
+                <p>Isso é um erro!</p>
+                <span>Já possui uma conta registrada?
+                    <Link to="/login">Login</Link>
+                </span>
+            </form>
+        </div>
     );
 };
