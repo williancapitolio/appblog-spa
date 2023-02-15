2c7c18ed47e0a9e53d6455e0ffa30f60fe13f4ed
Willian <williansilvap25@gmail.com>
2023-02-15 00:05:19 -0300

    add navbar and ur styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.png"
+
 export const Navbar = () => {
     return (
-        <>
-            <h1>
-                Navbar
-            </h1>
-        </>
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=art"><h6>Arte</h6></Link>
+                    <Link className="link" to="/?cat=science"><h6>Ciência</h6></Link>
+                    <Link className="link" to="/?cat=technology"><h6>Tecnologia</h6></Link>
+                    <Link className="link" to="/?cat=cinema"><h6>Cinema</h6></Link>
+                    <Link className="link" to="/?cat=design"><h6>Design</h6></Link>
+                    <Link className="link" to="/?cat=food"><h6>Alimentação</h6></Link>
+                    <span>Will</span>
+                    <span>Sair</span>
+                    <span className="write"><Link className="link" to="/write">Postar</Link></span>
+                </div>
+            </div>
+        </div>
     );
 };
