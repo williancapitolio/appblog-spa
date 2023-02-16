731234b9ffcc370175f779d809aefe50eb7fab43
Willian <williansilvap25@gmail.com>
2023-02-15 23:45:48 -0300

    navbar logo link to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <div className="navbar">
             <div className="container">
                 <div className="logo">
-                    <img src={Logo} alt="Logo" />
+                    <Link className="link" to="/"><img src={Logo} alt="Logo" /></Link>
                 </div>
                 <div className="links">
                     <Link className="link" to="/?cat=art"><h6>Arte</h6></Link>
