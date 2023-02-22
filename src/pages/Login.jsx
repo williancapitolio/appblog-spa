b033b471bbd8beb494e50b13f63287da32113ead
Willian <williansilvap25@gmail.com>
2023-02-21 23:33:31 -0300

    add login page with backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { api } from "../services/Api";
+import { useNavigate, Link } from "react-router-dom";
 
 export const Login = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    });
+    const [err, setErr] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await api.post("/auth/login", inputs);
+            navigate("/")
+        } catch (err) {
+            setErr(err.response.data)
+        };
+    };
+
     return (
         <div className="auth">
             <h1>Login</h1>
             <form>
-                <input type="text" name="username" placeholder="Usuário" required />
-                <input type="password" name="password" placeholder="Senha" required />
-                <button>Login</button>
-                <p>Isso é um erro!</p>
+                <input type="text" name="username" placeholder="Usuário" onChange={handleChange} required />
+                <input type="password" name="password" placeholder="Senha" onChange={handleChange} required />
+                <button onClick={handleSubmit}>Login</button>
+                {err && <p>{err}</p>}
                 <span>Ainda não possui uma conta?
                     <Link to="/register">Registre-se</Link>
                 </span>
