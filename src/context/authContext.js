db28e3953fcc15b46641df68e8e1b95c04e29f41
Willian <williansilvap25@gmail.com>
2023-02-22 00:05:28 -0300

    create auth context for login and logout

---
+++ src/context/authContext.js
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from "react";
+import { api } from "../services/Api";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+
+    const login = async (inputs) => {
+        const res = await api.post("/auth/login", inputs);
+        setCurrentUser(res.data);
+    };
+
+    const logout = async (inputs) => {
+        await api.post("/auth/logout", inputs);
+        setCurrentUser(null);
+    };
+
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(currentUser));
+    }, [currentUser]);
+
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { AuthContextProvider } from "./context/authContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
     </StrictMode>
 );
